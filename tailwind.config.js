33e7a22b0106d6a1b4fec33f6c15176f0cd3fa7c
Balamurugan <70791845+msbalacse@users.noreply.github.com>
2022-07-18 15:11:35 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Ubuntu : 'Ubuntu, sans-serif'
+    }
+  },
+  plugins: [],
+}
